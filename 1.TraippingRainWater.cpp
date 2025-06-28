32e3a7a53f7b74d008fad2fcc8d1bd5da0627025
Namrata Singh <144470271+namrata-singh-student@users.noreply.github.com>
2025-06-28 19:29:27 +0530

    Add files via upload
    Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
    
    Input Format
    
    The first line contains an integer n — the number of bars (size of the elevation map).
    The second line contains n space-separated non-negative integers — the heights of the bars.
    Constraints
    
    n == height.length
    1 <= n <= 2 * 10^4
    0 <= height[i] <= 10^5
    Output Format
    
    A single integer — the total amount of rain water trapped.
    Sample Input 0
    
    12
    0 1 0 2 1 0 1 3 2 1 2 1
    Sample Output 0
    
    6

---
+++ 1.TraippingRainWater.cpp
@@ -0,0 +1,126 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+  int height[n];
+  for (int i = 0; i < n; i++)
+  {
+    cin >> height[i];
+  }
+  int leftMax[n], rightMax[n];
+  leftMax[0] = height[0];
+  for (int i = 1; i < n; i++)
+  {
+    leftMax[i] = max(leftMax[i - 1], height[i]);
+  }
+  rightMax[n - 1] = height[n - 1];
+  for (int i = n - 2; i >= 0; i--)
+  {
+    rightMax[i] = max(rightMax[i + 1], height[i]);
+  }
+  int sum = 0;
+  for (int i = 0; i < n; i++)
+  {
+    int h = (min(leftMax[i], rightMax[i])) - height[i];
+    sum += h;
+  }
+  cout << sum;
+  return 0;
+}
+
+// int trapRainWater(vector<int>& height) {
+//     int n = height.size();
+//     if (n == 0) return 0;
+
+//     vector<int> leftMax(n), rightMax(n);
+//     leftMax[0] = height[0];
+//     for (int i = 1; i < n; i++) {
+//         leftMax[i] = max(leftMax[i - 1], height[i]);
+//     }
+//     rightMax[n - 1] = height[n - 1];
+//     for (int i = n - 2; i >= 0; i--) {
+//         rightMax[i] = max(rightMax[i + 1], height[i]);
+//     }
+//     int sum = 0;
+//     for (int i = 0; i < n; i++) {
+//         int h = min(leftMax[i], rightMax[i]) - height[i];
+//         sum += h;
+//     }
+
+//     return sum;
+// }
+
+// int main() {
+//     int n;
+//     cin >> n;
+//     vector<int> height(n);
+//     for (int i = 0; i < n; i++) {
+//         cin >> height[i];
+//     }
+
+//     int result = trapRainWater(height);
+//     cout << result << endl;
+
+//     return 0;
+// }
+
+// class Solution {
+// public:
+// int trap(vector<int>& height) {
+//     int n = height.size();
+//     //if (n == 0) return 0;
+
+//     vector<int> leftMax(n), rightMax(n);
+//     leftMax[0] = height[0];
+//     for (int i = 1; i < n; i++) {
+//         leftMax[i] = max(leftMax[i - 1], height[i]);
+//     }
+//     rightMax[n - 1] = height[n - 1];
+//     for (int i = n - 2; i >= 0; i--) {
+//         rightMax[i] = max(rightMax[i + 1], height[i]);
+//     }
+//     int sum = 0;
+//     for (int i = 0; i < n; i++) {
+//         int h = min(leftMax[i], rightMax[i]) - height[i];
+//         sum += h;
+//     }
+
+//     return sum;
+// }
+
+// vector<int>getLeftMaxArray(vector<int>&height,int n){
+//     vector<int>leftMax(n);
+//     leftMax[0]=height[0];
+//     for(int i=1;i<n;i++){
+//         leftMax[i]=max(leftMax[i-1],height[i]);
+//     }
+//     return leftMax;
+// }
+// vector<int>getRightMaxArray(vector<int>&height,int n){
+//     vector<int>rightMax(n);
+//     rightMax[n-1]=height[n-1];
+//     for(int i=n-2;i>=0;i--){
+//         rightMax[i]=max(rightMax[i+1],height[i]);
+//     }
+//     return rightMax;
+// }
+// int trap(vector<int>& height) {
+//     int n=height.size();
+//     vector<int>leftMax=getLeftMaxArray(height,n);
+//     vector<int>rightMax=getRightMaxArray(height,n);
+//     int sum=0;
+//     for(int i=0;i<n;i++){
+//         int h= (min(leftMax[i],rightMax[i]))-height[i];
+//         sum+=h;
+//     }
+//     return sum;
+// }
+//}
+;
