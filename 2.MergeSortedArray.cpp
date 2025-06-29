7d70863b6c7af18f453bae528a296a6ec3960b8b
Namrata Singh <144470271+namrata-singh-student@users.noreply.github.com>
2025-06-29 23:14:00 +0530

    Add files via upload

---
+++ 2.MergeSortedArray.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    void merge(vector<int> &A, int m, vector<int> &B, int n)
+    {
+        int index = m + n - 1;
+        int i = m - 1, j = n - 1;
+        while (i >= 0 && j >= 0)
+        {
+            if (A[i] >= B[j])
+            {
+                A[index] = A[i];
+                index--;
+                i--;
+            }
+            else
+            {
+                A[index] = B[j];
+                index--;
+                j--;
+            }
+        }
+        while (j >= 0)
+        {
+            A[index] = B[j];
+            index--;
+            j--;
+        }
+    }
+};
