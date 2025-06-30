cf5f21fb01e6f522789bfcce356171d38c1717e4
Namrata Singh <144470271+namrata-singh-student@users.noreply.github.com>
2025-07-01 02:37:34 +0530

    Update 6.Longest-Common-Prefix.cpp

--- 6.Longest-Common-Prefix.cpp
+++ 6.Longest-Common-Prefix.cpp
@@ -13,3 +13,19 @@ public:
         return str1.substr(0,idx);
     }
 };
+--------------------------------------------------------
+    //java code
+    class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0) return "";
+        Arrays.sort(strs);
+        String str1 = strs[0];
+        String str2 = strs[strs.length-1];
+        int index=0;
+        while(index <str1.length() && index < str2.length() && 
+        str1.charAt(index) == str2.charAt(index)){
+            index++;
+        }
+        return str1.substring(0,index);
+    }
+}
