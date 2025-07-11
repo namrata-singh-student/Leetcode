d5bc64209e7647df6ff10c03c69cadd6aa3ef1b1
Namrata Singh <144470271+namrata-singh-student@users.noreply.github.com>
2025-07-11 20:32:42 +0530

    31.Find_Smallest_Letter_Greater_Than_Target.cpp

---
+++ 31.Find_Smallest_Letter_Greater_Than_Target.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int n = letters.size();
+        int l = 0, r = n-1;
+        int pos = -1;
+        while(l<=r){
+            int mid = (l+r)/2;
+            if(letters[mid] > target){
+                pos = mid;
+                r = mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        if(pos == -1){
+            return letters[0];
+        }else{
+            return letters[pos];
+        }
+    }
+};
