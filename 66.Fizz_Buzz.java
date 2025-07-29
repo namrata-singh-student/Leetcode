1e8ab292393c9e91684cff80c36a0ad58a4998f2
Namrata Singh <144470271+namrata-singh-student@users.noreply.github.com>
2025-07-29 23:58:13 +0530

    66.Fizz_Buzz.java

--- 66.Fizz_Buzz.java
+++ 66.Fizz_Buzz.java
@@ -30,3 +30,24 @@ class Solution {
         return result;
     }
 }
+
+
+//Java Code-----------------------------------------
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string>result;
+        for(int i = 1 ; i<=n ;i++){
+            if(i%3 == 0 && i%5 == 0){
+                result.push_back("FizzBuzz");
+            }else if(i%3 == 0){
+                result.push_back("Fizz");
+            }else if(i%5 == 0){
+                result.push_back("Buzz");
+            }else{
+                result.push_back(to_string(i));
+            }
+        }
+        return result;
+    }
+};
