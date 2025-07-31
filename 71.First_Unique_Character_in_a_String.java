827d2af86689fc05b08f115411dd585cf62db2bf
Namrata Singh <144470271+namrata-singh-student@users.noreply.github.com>
2025-08-01 01:01:48 +0530

    71.First_Unique_Character_in_a_String.java

--- 71.First_Unique_Character_in_a_String.java
+++ 71.First_Unique_Character_in_a_String.java
@@ -61,3 +61,21 @@ class Solution {
         return -1;
     }
 }
+// In java we can access string by converting it into char during loop also
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+        // Count frequency of each character
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        // Find first character with frequency 1
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
