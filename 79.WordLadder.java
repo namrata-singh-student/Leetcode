fc15943a7681938bf5342b889c83e27d1494dfd5
Namrata Singh <144470271+namrata-singh-student@users.noreply.github.com>
2025-08-15 23:13:07 +0530

    79.WordLadder.java
    LeetCode Hard Problem

--- 79.WordLadder.java
+++ 79.WordLadder.java
@@ -67,3 +67,37 @@ class Solution {
         return 0;
     }
 }
+
+//c++
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        int n = wordList.size();
+        queue<pair<string,int>>q;
+        q.push({beginWord , 1});
+        unordered_set<string>st(wordList.begin() , wordList.end());
+        st.erase(beginWord);
+        while(!q.empty()){
+            string word = q.front().first;
+            int steps = q.front().second;
+            q.pop();
+            if(word == endWord){
+                return steps;
+            }
+            for(int i = 0 ;i<word.length(); i++){
+                char originalChar = word[i];
+                for(char ch = 'a' ; ch <= 'z' ; ch++){
+                    word[i] = ch;
+                    if(st.find(word) != st.end()){
+                        st.erase(word);
+                        q.push({word , steps+1});
+                    }
+                }
+                word[i] = originalChar;
+            }
+        }
+        return 0;
+    }
+};
+
+
